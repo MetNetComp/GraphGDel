fc774b735248a7e26d866efd63c6cd7a8fc43bc8
Ziwei Yang <85477529+yangziwei96@users.noreply.github.com>
2026-02-27 23:54:09 +0900

    Delete model directory

--- model/freeze_model.py
+++
@@ -1,8 +0,0 @@
-import torch.nn as nn
-
-def freeze_model(model: nn.Module):
-    """
-    Freezes the parameters of the model so they will not be updated during training.
-    """
-    for param in model.parameters():
-        param.requires_grad = False
